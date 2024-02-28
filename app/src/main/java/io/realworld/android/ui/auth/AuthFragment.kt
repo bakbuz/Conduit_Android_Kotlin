027e88a65a8ce869ab09278541c15ecfb323149c
Bayram Akbuz <bayram.akbuz@gmail.com>
2024-02-28 03:21:42 +0300

    Material Design 3

--- app/src/main/java/io/realworld/android/ui/auth/AuthFragment.kt
+++ app/src/main/java/io/realworld/android/ui/auth/AuthFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.NavHost
@@ -18,6 +19,7 @@ class AuthFragment : Fragment() {
 
     private var _binding: FragmentAuthBinding? = null
     private var navController: NavController? = null
+    //private var screenTitle:TextView?=null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +27,7 @@ class AuthFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentAuthBinding.inflate(inflater, container, false)
+        //screenTitle = _binding?.root?.findViewById(R.id.authTitleText);
         return _binding?.root
     }
 
@@ -32,14 +35,17 @@ class AuthFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         navController = _binding?.let { Navigation.findNavController(it.root.findViewById(R.id.authFragmentNavHost)) }
+
         _binding?.authTabLayout?.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 when(tab?.position) {
                     0 -> {
                         navController?.navigate(R.id.gotoLoginFragment)
+                        //screenTitle?.setText(R.string.auth_title_login)
                     }
                     1 -> {
                         navController?.navigate(R.id.gotoSignupFragment)
+                        //screenTitle?.setText(R.string.auth_title_register)
                     }
                 }
             }
