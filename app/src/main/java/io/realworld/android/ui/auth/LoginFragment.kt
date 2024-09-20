25b987fccb03d3cb4148bb64f7df72e5bd8bcf84
Bayram Akbuz <bayram.akbuz@gmail.com>
2024-09-20 18:44:49 +0300

    gradle upgrade

--- app/src/main/java/io/realworld/android/ui/auth/LoginFragment.kt
+++ app/src/main/java/io/realworld/android/ui/auth/LoginFragment.kt
@@ -26,7 +26,7 @@ class LoginFragment : Fragment() {
         return _binding?.root
     }
 
-    // shared prefenrecnes 'a sakla
+    // shared preferences 'a sakla
     // sadece token değerini encrypt ederek sakla
     // uygulama initialize edilirkn o token değerine sahip kullanıcının bilgilerini çek
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
